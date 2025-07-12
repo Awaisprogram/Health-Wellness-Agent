09bb631bbf0fff96b8c92306502cc5b60110e0ea
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-07-12 22:18:36 +0500

    Update main.py

--- health_wellness_agent/main.py
+++ health_wellness_agent/main.py
@@ -1,6 +1,7 @@
 # main.py
 import chainlit as cl
-from agents import AsyncOpenAI,OpenAIChatCompletionsModel,RunConfig 
+from agents import RunConfig
+from model import model,provider
 import os
 from dotenv import load_dotenv
 from context import UserContext
@@ -9,20 +10,6 @@ from utils.streaming import stream_response
 from hooks import MyHooks 
 from typing import Optional, Dict
 
-
-load_dotenv()
-
-gemini_api_key = os.getenv("GEMINI_API_KEY")
-
-provider = AsyncOpenAI(
-  api_key = gemini_api_key,
-  base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-)
-
-model = OpenAIChatCompletionsModel(
-  model = "gemini-2.0-flash",
-  openai_client = provider,
-)
 
 start_hook = MyHooks()
 
