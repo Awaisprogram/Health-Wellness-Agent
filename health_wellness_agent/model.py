9ad9c6eea8d191f7b17aef6f7aca6da6f995287a
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-07-12 22:16:43 +0500

    Create model.py

---
+++ health_wellness_agent/model.py
@@ -0,0 +1,15 @@
+from agents import OpenAIChatCompletionsModel, AsyncOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY environment variable is not set.")
+
+provider = AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+model = OpenAIChatCompletionsModel(openai_client=provider, model="gemini-2.0-flash")
